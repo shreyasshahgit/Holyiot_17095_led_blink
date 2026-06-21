19a545e5c9dfa057647156dd6c09b33e7b369bd5
shreyasshahgit <131869374+shreyasshahgit@users.noreply.github.com>
2026-06-21 15:06:04 +0530

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -1,14 +1,30 @@
 #include <zephyr/kernel.h>
-#include <hal/nrf_gpio.h>
+#include <zephyr/drivers/gpio.h>
 
-#define LED_PIN 29
+#define LED_NODE DT_ALIAS(led0)
+#define LED_LABEL DT_LABEL(LED_NODE)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 int main(void)
 {
-    nrf_gpio_cfg_output(LED_PIN);
+    int ret;
+
+    if (!gpio_is_ready_dt(&led)) {
+        printk("Error: LED device is not ready\n");
+        return 0;
+    }
+
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        printk("Error: Could not configure LED pin\n");
+        return 0;
+    }
+
+    printk("LED blinking on %s (P0.29)\n", LED_LABEL);
 
     while (1) {
-        nrf_gpio_pin_toggle(LED_PIN);
+        gpio_pin_toggle_dt(&led);
         k_msleep(500);
     }
 }
